b2458326297d0507fd786e894f63dd5502629d9e
Karim Aboud <online.karim1@gmail.com>
2023-02-09 16:29:41 +0100

    & fix

--- src/main/java/fr/capeb/backend/riskevaluator/projection/ScoreMoyenEntrepriseResponse.java
+++ src/main/java/fr/capeb/backend/riskevaluator/projection/ScoreMoyenEntrepriseResponse.java
@@ -1,7 +1,9 @@
 package fr.capeb.backend.riskevaluator.projection;
-public class ScoreMoyenEntrepriseResponse {
+public class ScoreMoyenEntrepriseResponse implements ScoreMoyenEntreprise {
 	
 	private String nomEntreprise;
+	
+	private String taille;
 	
 	private String thematique;
 	
@@ -11,5 +13,26 @@ public class ScoreMoyenEntrepriseResponse {
 		this.thematique = scoreMoyenEntreprise.getThematique();
 		this.scoreMoyen = scoreMoyenEntreprise.getScoreMoyen();
 		this.nomEntreprise = scoreMoyenEntreprise.getNomEntreprise();
+		this.taille = scoreMoyenEntreprise.getTaille();
+	}
+	
+	@Override
+	public String getNomEntreprise() {
+		return nomEntreprise;
+	}
+	
+	@Override
+	public String getTaille() {
+		return taille;
+	}
+	
+	@Override
+	public String getThematique() {
+		return thematique;
+	}
+	
+	@Override
+	public double getScoreMoyen() {
+		return scoreMoyen;
 	}
 }
